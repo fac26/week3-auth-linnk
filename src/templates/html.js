694e862b604d8534171ffd485185529008114c55
LauraK0 <laurakeating@live.co.uk>
2023-01-31 15:08:52 +0000

    html template added

--- src/templates/html.js
+++ src/templates/html.js
@@ -1,3 +1,36 @@
 //html
 //nav bar which holds home-link, (logout/add)||(login/signin)
 //${title} for h1, home-link: '/', signed-in: true|false} and ${content}=>secrets,forms
+
+function html({ title, homeLink, signedIn, content }) {
+    return /*html*/ `
+  <!doctype html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <title>${title}</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+    <header>
+    <nav role="navigation" class="nav-bar">
+    <a href="${homeLink}">Home</a>
+      <ul class="nav-links">
+      ${
+          signedIn
+              ? /*html*/ `<a href="/add-secret">Add Secret</a> <form method="POST" action="/log-out"><button class="Button">Log out</button></form>`
+              : /*html*/ `<a href="/sign-up">Sign up</a> or <a href="/log-in">log in</a>`
+      }
+      </ul>
+    </nav>
+  </header>
+        <main>
+          ${content}
+        </main>
+      </div>
+    </body>
+  </html>
+`;
+}
+
+module.exports = { html };
