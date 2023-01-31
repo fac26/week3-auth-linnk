40d46f72004e6f8177fe4b82944787f9a96c52d0
ighellache <igeghellache@gmail.com>
2023-01-31 15:03:13 +0000

    filled in companies

--- src/model/companies.js
+++ src/model/companies.js
@@ -1,1 +1,19 @@
 //add new company to db, we get the name of company as argument
+
+const db = require("../database/db")
+
+const insert_company = db.prepare(`
+    INSERT INTO companies(name)
+    VALUES(
+        $name
+    )
+    RETURNING id
+    `)
+
+function addCompanyToDB(companyname) {
+    return insert_company.get(companyname);
+}
+
+module.exports = {addCompanyToDB}
+
+//iman
